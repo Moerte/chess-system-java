7e34f8476aa00e16044aa00726c77cf74a66cd1a
Moerte <nunoliveira86@gmail.com>
2019-06-11 17:46:51 +0100

    Defensive program

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,10 +1,9 @@
 package chess.pieces;
 
 import boardgame.Board;
-import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends chess.ChessPiece{
 
 	public King(Board board, Color color) {
 		super(board, color);
